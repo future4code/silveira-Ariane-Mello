1b222b53c37ec67ea89282a8cccc5b8188f39d9c
arianetier <arianedemello@outlook.com>
2022-03-22 17:46:22 -0300

    Componentes Funcionais, Props e Styled Components

--- modulo2/props-labedin/labedin/src/components/CardGrande/CardGrande.js
+++ modulo2/props-labedin/labedin/src/components/CardGrande/CardGrande.js
@@ -1,15 +1,40 @@
 import React from 'react';
-import './CardGrande.css'
+import styled from 'styled-components'
+
+const ComponenteEstilizado = styled.div`
+    display: flex;
+    align-items: center;
+    border: 1px solid black;
+    padding: 20px 10px;
+    margin-bottom: 10px;
+    height: 200px;
+`
+
+const Image = styled.img`
+    width: 70px;
+    margin-right: 10px;
+    border-radius: 50%;
+`
+
+const Descricao = styled.div`
+    margin-bottom: 15px;
+`
+
+const Nome = styled.h4`
+    display: flex;
+    flex-direction: column;
+    justify-items: flex-start;
+`
 
 function CardGrande(props) {
     return (
-        <div className="bigcard-container">
-            <img src={ props.imagem } />
-            <div>
-                <h4>{ props.nome }</h4>
+        <ComponenteEstilizado>
+            <Image src={ props.imagem } />
+            <Descricao>
+                <Nome>{ props.nome }</Nome>
                 <p>{ props.descricao }</p>
-            </div>
-        </div>
+            </Descricao>
+        </ComponenteEstilizado>
     )
 }
 
