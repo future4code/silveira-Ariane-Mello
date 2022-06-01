8fa170ce28d37864ee9932d62abaecd9af416c46
arianetier <arianedemello@outlook.com>
2022-06-01 18:08:58 -0300

    Fluxo de dados no Backend

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,23 @@
+export type Products = {
+    id: string,
+    name: string,
+    price: number
+};
+
+export const products: Products[] = [
+    {
+        "id": '1',
+        "name": 'Cupcake',
+        "price": 15
+    },
+    {
+        "id": '2',
+        "name": 'Brownie',
+        "price": 20
+    },
+    {
+        "id": '3',
+        "name": 'Petit Gateau',
+        "price": 30
+    }
+];
