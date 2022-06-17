f7a41078c2b06ffbcca329cad269fdd7912b1440
arianetier <arianedemello@outlook.com>
2022-06-17 10:24:02 -0300

    LabEcommerce

---
+++ modulo4/LabECommerce/src/index.ts
@@ -0,0 +1,21 @@
+import express, { Express } from 'express'
+import cors from 'cors'
+import { AddressInfo } from "net";
+import createUser from './endpoint/createUser';
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/user', createUser);
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost: ${address.port}`);
+    } else {
+        console.error(`Failure upon starting server.`);
+    }
+});
+
